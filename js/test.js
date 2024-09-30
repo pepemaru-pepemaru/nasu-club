d9ba14d6bce34e8c2788e245cb9bec69fe5e3c16
pepemaru-pepemaru <ikaruga20020803@gmail.com>
2024-10-01 05:30:35 +0900

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-  var pagetop = $('.pagetop');
+  var pagetop = $('#footer_pagetop');
     $(window).scroll(function () {
        if ($(this).scrollTop() > 100) {
             pagetop.fadeIn();
@@ -12,4 +12,30 @@ $(document).ready(function() {
            $('body, html').animate({ scrollTop: 0 }, 500);
               return false;
    });
+     const ham = $('#js-hamburger');
+     const nav = $('#js-nav');
+     ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+          ham.toggleClass('close'); // ハンバーガーメニューにhamburgerクラスを付け外し
+          nav.toggleClass('hamburger'); // ハンバーガーメニューにhamburgerクラスを付け外し
+
 });
+});
+
+
+
+// おすすめメニューの画像のクリックで大きく表示する部分（モーダルというらしい）
+
+$(function () {
+     $('.modal__open').each(function () {
+         $(this).on('click', function () {
+             var target = $(this).data('target');
+             var modal = document.getElementById(target);
+             console.log(modal)
+             $(modal).fadeIn();
+         });
+     });
+     $('.modal__close').on('click', function () {
+         $('.modal').fadeOut();
+         return false;
+     });
+ });
